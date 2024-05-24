7e4cf91fb782ad6dca18e98b79793d351132aab2
BHznJNs <441768875@qq.com>
2024-05-24 23:00:28 +0800

    contains-duplicate

--- examples/contains-duplicate/main.rs
+++ examples/contains-duplicate/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    // 
+use std::collections::HashSet;
+
+struct Solution;
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let original_len = nums.len();
+        let mut set = HashSet::new();
+        for num in nums {
+            set.insert(num);
+        }
+        return original_len != set.len();
+    }
 }
+
+fn main() {}
